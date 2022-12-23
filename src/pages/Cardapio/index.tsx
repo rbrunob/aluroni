fa64dfd0417c50c6e42cd645e0a13fe1236ea5ff
Bruno <bruno.bragaramos@gmail.com>
2022-12-22 23:29:34 -0300

    UPDATE ALURONI -- HEADER & SEARCH BAR

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,11 +1,27 @@
+import { useState } from 'react'
 import styles from './Cardapio.module.scss';
+import { ReactComponent as Logo } from "assets/logo.svg";
+import Buscador from './Buscador';
 
 export default function Cardapio() {
-    return(
+    const [busca, setBusca] = useState("");
+    return (
         <main>
             <nav className={styles.menu}>
-                <img src="" alt="Logo Aluroni" />
+                <Logo />
             </nav>
+            <header className={styles.header}>
+                <div className={styles.header__text}>
+                    A casa do código e da massa
+                </div>
+            </header>
+            <section className={styles.cardapio}>
+                <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+                <Buscador
+                    busca={busca}
+                    setBusca={setBusca}
+                />
+            </section>
         </main>
     )
 }
